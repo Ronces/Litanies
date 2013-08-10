05f05f5f935491fc7584c34d650cfdc50f22479e
Zaratan <denis.pasin@gmail.com>
2013-08-11 01:36:44 +0200

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Litanies::Application.config.secret_key_base = '16ca8afa55bf395913692bf51e937b6d75096f9fbc613d3761d7c60d1cde2efe767d8f483438d8aaf08d713b291ae3b101c4c3690c458e2b9dd4e96c2f77b630'
